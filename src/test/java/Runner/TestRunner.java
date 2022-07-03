88c0852522237acea1fc82e0b2c265d5af1ca97b
anil <anil.tambe@coditas.com>
2022-07-03 22:31:09 +0530

    updated TestRunner added cucumber-html report plugin

--- src/test/java/Runner/TestRunner.java
+++ src/test/java/Runner/TestRunner.java
@@ -9,7 +9,8 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(features ="src/test/java/Feature",
         glue = "stepDefinition",
-        plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"})
+        plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+                    "html:target/cucumber.html"})
 public class TestRunner {
 
 
