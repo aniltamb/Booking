f0624a7e80fc3f76cd05a5b31ce455b5138aecee
anil <anil.tambe@coditas.com>
2022-07-04 12:27:47 +0530

    changes

--- src/test/java/Runner/TestRunner.java
+++ src/test/java/Runner/TestRunner.java
@@ -1,18 +1,17 @@
 package Runner;
 
 
+
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
-import org.junit.AfterClass;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features ="src/test/java/Feature",
         glue = "stepDefinition",
-        plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
-                    "html:target/cucumber.html",
-                "json:target/cucumber.json"
-        })
+plugin = {"pretty","html:target/cucumber.html","json:target/cucumber.json",
+        "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"})
+
 public class TestRunner {
 
 
